152cbd2e2f5b2fe5153da4906ff2c79d7eaa7be3
Uğur Oğuzhan Obuz <ugurobuz@gmail.com>
2025-02-26 22:12:28 +0300

    Mantine installation and React tree shaking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+import "@mantine/core/styles.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript/>
+      </head>
+      <body>
+        <MantineProvider>
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
