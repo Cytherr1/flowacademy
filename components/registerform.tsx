fbf67ab2fdea6458896a93f13b89199b840fcefb
Uğur Oğuzhan Obuz <ugurobuz@gmail.com>
2025-03-18 03:00:08 +0300

    bcrypt and register form init

--- components/registerform.tsx
+++ components/registerform.tsx
@@ -1,7 +1,53 @@
 "use client";
+import { zodResolver } from 'mantine-form-zod-resolver';
+import { useForm } from '@mantine/form';
+import { Button, Group, Paper, PasswordInput, Stack, TextInput } from '@mantine/core';
+import { userLogin } from '@/lib/actions/auth';
+import { loginSchema } from '@/lib/schema';
 
-export default function RegisterForm() {
+export default function SignInForm() {
+  
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validate: zodResolver(loginSchema),
+    onSubmitPreventDefault: 'validation-failed',
+  });
+
   return (
-	<div>registerform</div>
+    <form 
+      action={ async (formData: FormData) => {
+        form.validate()
+        if(form.isValid()) {
+          await userLogin(formData);
+        }
+    }}>
+      <Paper
+        withBorder
+        component={Stack}
+        p="md"
+        w={350}
+      >
+        <TextInput
+          label="Email"
+          name='email'
+          placeholder="flowacademy@email.com"
+          key={form.key("email")}
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          label="Password"
+          name='password'
+          key={form.key("password")}
+          {...form.getInputProps('password')}
+        />
+        <Group justify="flex-end" mt="md">
+          <Button variant="default" type="submit">Sign in</Button>
+        </Group>
+      </Paper>
+    </form>
   )
 }
