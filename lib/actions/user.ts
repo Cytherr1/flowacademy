665bf8820eeac888e763f894e534c58f3a708939
Uğur Oğuzhan Obuz <ugurobuz@gmail.com>
2025-03-21 17:55:06 +0300

    edit form correction

--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -17,10 +17,10 @@ export const editUser = async ( formData: FormData, session: Session ) => {
       id: user?.id
 		},
 		data: {
-      name: (formData.get("name") ?? user?.name) as string,
-			username: (formData.get("username") ?? user?.username) as string,
-      image: (formData.get("image") ?? user?.image) as string,
-			password: formData.get("password") !== null ? await hash(formData.get('password') as string, salt) : user?.password
+      name: formData.get("name") !== "" ? formData.get("name") as string : user?.name as string,
+			username: formData.get("username") !== "" ? formData.get("username") as string : user?.username as string,
+      image: formData.get("image") !== "" ? formData.get("image") as string : user?.image as string,
+			password: formData.get("password") !== "" ? await hash(formData.get('password') as string, salt) : user?.password
 		}
 	})
 }
